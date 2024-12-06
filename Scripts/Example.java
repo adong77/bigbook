1365238c16efb6e3004a3bdedc3e8a38a7d862f5
adong77 <youdng@hotmail.com>
2024-12-06 15:29:00 +0800

    dfas

---
+++ Scripts/Example.java
@@ -0,0 +1,7 @@
+/* This is simple Java program. */
+class Example {
+    // A Java program begins with a call to main()
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
